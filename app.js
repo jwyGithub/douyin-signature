a537b9ae1dc5afa6798d09f3b9d41ba1a5f807e1
jiangwy <jiangwy@staryea.com>
2023-11-02 15:16:01 +0800

    chore: add index.html

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 import http from 'node:http';
 import path from 'node:path';
+import fs from 'node:fs';
 import { getRequestUrl, getRouteUrl, request } from './src/shared/shared.js';
 import { getSign } from './src/core/getSign.js';
 
@@ -11,6 +12,9 @@ import { getSign } from './src/core/getSign.js';
  * @typedef {{[key:string]: RouteExcuter }} Route
  */
 const BASE_URL = 'https://www.iesdouyin.com/web/api/v2/aweme/iteminfo/';
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const STATIC_PATH = path.resolve(__dirname, './static');
 
 /** @type {Route} */
 const routes = {
@@ -40,7 +44,8 @@ const routes = {
         try {
             const location = req.body.location;
 
-            const { base: item_ids } = path.parse(location);
+            const { dir } = path.parse(location);
+            const { base: item_ids } = path.parse(dir);
 
             if (item_ids) {
                 res.statusCode = 200;
@@ -116,12 +121,20 @@ const routes = {
                 const [videoUrl] = url_list;
                 const video_title = video_info.item_list[0].desc;
                 res.statusCode = 200;
-                res.end(
-                    JSON.stringify({
-                        video_url: videoUrl.replace('playwm', 'play'),
-                        video_title
-                    })
-                );
+                const _videoUrl = videoUrl.replace('playwm', 'play');
+                request(_videoUrl, {
+                    method: 'GET',
+                    headers: {
+                        'User-Agent': 'ua' // 模拟手机连接
+                    }
+                }).then(({ headers }) => {
+                    res.end(
+                        JSON.stringify({
+                            video_url: headers.location,
+                            video_title
+                        })
+                    );
+                });
             })
             .catch(error => {
                 console.error(error);
@@ -143,7 +156,15 @@ function router(url, req, res) {
     });
     req.on('end', () => {
         req.body = req.method === 'GET' ? {} : JSON.parse(bodyData.toString() || '');
-        routes[route](url, req, res);
+        if (!route) {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html');
+            const index_path = path.resolve(STATIC_PATH, './index.html');
+            const content = fs.readFileSync(index_path);
+            res.end(content);
+        } else {
+            routes[route] && routes[route](url, req, res);
+        }
     });
     req.on('error', error => {
         console.error(error);
